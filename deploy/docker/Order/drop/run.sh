510edc9ac6c7f187c9e40efa47478ea85c2642c3
DANIEL CANTOS GARCIA <Daniel.Cantos@alu.uclm.es>
2021-11-09 18:56:18 +0100

    run.sh y application.properties arreglado  [ci skip]

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,2 +1,8 @@
-mongodb.host: mongo
-mongodb.database: ordering
+while ! curl http://mongo:27017/
+do
+    echo "$(date) - still trying"
+    sleep 1
+done
+echo "$(date) - connected successfully"
+
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties
